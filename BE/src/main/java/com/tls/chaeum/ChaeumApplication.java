3a511133ac5f449a52e0fa6e6677cb4ca13ecb22
gunhoo <gunhoo2016@gmail.com>
2023-09-05 12:08:41 +0900

    init: BE gradle setting
    BE springboot gradle 설정

---
+++ BE/src/main/java/com/tls/chaeum/ChaeumApplication.java
@@ -0,0 +1,13 @@
+package com.tls.chaeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaeumApplication.class, args);
+	}
+
+}
