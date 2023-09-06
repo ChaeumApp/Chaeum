286116db7b25aaabe6a83e32d1b00119640a722d
gunhoo <gunhoo2016@gmail.com>
2023-09-06 11:48:59 +0900

    fix: basepackage 설정

--- BE/src/main/java/com/tls/chaeum/ChaeumApplication.java
+++ BE/src/main/java/com/tls/chaeum/ChaeumApplication.java
@@ -3,7 +3,7 @@ package com.tls.chaeum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.tls")
 public class ChaeumApplication {
 
 	public static void main(String[] args) {
