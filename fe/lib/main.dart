1a16be2b08f900b6cb9d6ccfc3898ac74d19c1f5
오연주 <siunaus224@gmail.com>
2023-09-07 16:45:47 +0900

    feat: 검색창, 하단바, 메인 배너 구현

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import './repeat/bottom.dart';
+import './main/mainbody.dart';
+
 
 void main() {
+  // 상태바 색상 변경하는 코드
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark
+    )
+  );
   runApp(MaterialApp(
     theme: ThemeData(
-      fontFamily: "Pretendard"
+      fontFamily: "Pretendard",
     ),
       home: const Main())
   );
@@ -14,9 +26,25 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('채움'),),
+    return DefaultTabController(
+        initialIndex: 2,
+        length: 5,
+        child: Scaffold(
+          body: SafeArea(
+            child: TabBarView(
+              children: [
+                Center(child: Text('식재료')),
+                Center(child: Text('레시피')),
+                Mainb(),
+                Center(child: Text('검색')),
+                Center(child: Text('내정보')),
+              ],
+            ),
+          ),
+          extendBodyBehindAppBar: true,
+          bottomNavigationBar: Bottom(),)
+
     );
   }
 }
+
---
+++ fe/lib/main/maincategory.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MainCategory extends StatelessWidget {
+  const MainCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var category = {
+      '' : '과일',
+      '' : '곡류',
+      '' : '채소',
+      '' : '수산',
+      '' : '축산',
+      '' : '견과',
+      '' : '',
+      '' : '',
+      '' : '',
+    };
+
+    return Container();
+  }
+}
