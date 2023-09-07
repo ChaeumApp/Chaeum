88d8ca881e9de8b3782751e012640afc3d7a1f8c
오연주 <siunaus224@gmail.com>
2023-09-07 17:44:56 +0900

    feat: 메인 페이지 대분류 추가

--- fe/lib/main/maincategory.dart
+++ fe/lib/main/maincategory.dart
@@ -5,18 +5,19 @@ class MainCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var category = {
-      '' : '과일',
-      '' : '곡류',
-      '' : '채소',
-      '' : '수산',
-      '' : '축산',
-      '' : '견과',
-      '' : '',
-      '' : '',
-      '' : '',
-    };
-
-    return Container();
+    var category = [
+      {'name' : '과일', 'image' : 'orange'},
+      {'name' : '곡류/견과', 'image' : 'rice'},
+      {'name' : '채소', 'image' : 'cabbage'},
+      {'name' : '수산', 'image' : 'seafood'},
+      {'name' : '축산', 'image' : 'meat'},
+      {'name' : '유제품', 'image' : 'milk'},
+      {'name' : '김치', 'image' : 'kimchi'},
+      {'name' : '면/통조림', 'image' : 'noodle'},
+      {'name' : '양념', 'image' : 'oil'},
+      {'name' : '베이커리/잼', 'image' : 'bakery'}
+    ];
+    
+    return Text('하.. 미치겠군...');
   }
 }
