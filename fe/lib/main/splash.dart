a75c399b3baa35b3dd13a129f529298b5e139acf
오연주 <siunaus224@gmail.com>
2023-09-08 11:35:25 +0900

    feat: 스플래시 스크린 구현
    Former-commit-id: 9c8604ab5a896903c27711517c153442375e25ae

---
+++ fe/lib/main/splash.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../main.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode((SystemUiMode.immersive));
+
+    Future.delayed(Duration(seconds: 5), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (_)=> const Main(),
+          ));
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xffFEFFFF),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        margin: EdgeInsets.fromLTRB(0, 0, 0, 35),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/splash_logo.png',
+              width: 70,),
+              Image.asset('assets/images/splash.gif',
+              width: 200,)
+            ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.fromLTRB(0, 0, 0, 35),
+        child: Text('ⓒ The LEGEND SIX. SSAFY',
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+        ),
+        textAlign: TextAlign.center,),
+      ),
+    );
+  }
+}
