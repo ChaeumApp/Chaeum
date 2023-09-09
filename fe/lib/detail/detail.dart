54cc46fe6f12994371107e7f335d4ba21ecb5e99
Yeoni <siunaus224@gmail.com>
2023-09-10 02:01:16 +0900

    feat: 상세 페이지 탭바까지 구현
    Former-commit-id: 7a7eb4cec29083c7f2a1b5141ea544a4fbc7940d

--- fe/lib/detail/detail.dart
+++ fe/lib/detail/detail.dart
@@ -1,35 +1,100 @@
 import 'package:flutter/material.dart';
+import './detailtab.dart';
 
 class Detail extends StatelessWidget {
-  const Detail({super.key, this.category});
+  Detail({super.key, this.category});
+
   final category;
+
+  var data = {'saleper': 10, 'salewon': 300, 'salerank': 1};
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        body: CustomScrollView(
-          slivers : [
-            SliverAppBar(
-              backgroundColor: Colors.white,
-              expandedHeight: 60,
-              elevation: 0,
-              title: Text('${category}',
-                style: TextStyle(
-                 color: Colors.black,
-                 fontWeight: FontWeight.w700),),
-              pinned: true,
-              floating: false,
-              leading: const BackButton(
-                     color: Colors.black,
-                   ),
-              centerTitle: true,
-            ),
-            SliverToBoxAdapter(
-              child: Image.asset('assets/images/temporary/sample.jpg'),
-            )
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          '$category',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
         ),
-      );
+        leading: const BackButton(
+          color: Colors.black,
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            'assets/images/temporary/sample.png',
+            height: 163,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            height: 93,
+            width: double.infinity,
+            padding: EdgeInsets.only(left: 20),
+            margin: EdgeInsets.only(bottom: 5),
+            decoration: BoxDecoration(
+                border: Border(
+              bottom: BorderSide(color: Color(0xffB3B3B3), width: 0.5),
+            )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 3),
+                  child: Text(
+                    '$category',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                Text(
+                  '어제보다 ${data['saleper']}%(${data['salewon']}원) 더 비싸졌어요.',
+                  style: TextStyle(
+                    color: Color(0xff73324C),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text('최근 3개월 중 ${data['salerank']}번째로 비싼 날이에요.',
+                    style: TextStyle(
+                      color: Color(0xff73324C),
+                      fontWeight: FontWeight.w700,
+                    )),
+              ],
+            ),
+          ),
+          DefaultTabController(
+            length: 3,
+            child: Expanded(
+              child: Column(
+                children: [
+                  DetailTab(),
+                  Expanded(
+                    child: TabBarView(children: [
+                      Container(
+                        color: Colors.blue,
+                      ),
+                      Container(
+                        color: Colors.red,
+                      ),
+                      Container(
+                        color: Colors.black,
+                      ),
+                    ]),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
