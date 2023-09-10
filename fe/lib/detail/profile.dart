f5ae35f33943814e9c01ca357ae407ab69ed0211
Yeoni <siunaus224@gmail.com>
2023-09-10 15:10:42 +0900

    feat: 상세 페이지 column으로 변경
    Former-commit-id: 44cf7ca3574bb1077dd0d2b6e765a36996e6ef3f

---
+++ fe/lib/detail/profile.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  ProfileView({super.key, this.category});
+  final category;
+
+  var data = {'saleper': 10, 'salewon': 300, 'salerank': 1};
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          '$category',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border, color: Colors.black),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            'assets/images/temporary/sample.png',
+            height: 163,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            height: 93,
+            width: double.infinity,
+            padding: EdgeInsets.only(left: 20),
+            // margin: EdgeInsets.only(bottom: 5),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Color(0xffB3B3B3), width: 0.5),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 3),
+                  child: Text(
+                    '$category',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                Text(
+                  '어제보다 ${data['saleper']}%(${data['salewon']}원) 더 비싸졌어요.',
+                  style: TextStyle(
+                    color: Color(0xff73324C),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '최근 3개월 중 ${data['salerank']}번째로 비싼 날이에요.',
+                  style: TextStyle(
+                    color: Color(0xff73324C),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
