d217f19363363baff6c13a940d8031168022b78e
오연주 <siunaus224@gmail.com>
2023-09-11 15:11:39 +0900

    feat: 상세페이지 추천상품 페이지 목록
    Former-commit-id: 11aa8f90abe8777e3b851e8edf2453efa2beeadb

--- fe/lib/detail/profile.dart
+++ fe/lib/detail/profile.dart
@@ -9,7 +9,6 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
