ed325bb0e2e06959d32dd27b212165a05eb55127
박은규 <parkeg1223@gmail.com>
2023-09-12 17:52:35 +0900

    style: unused import 구문 삭제

--- BE/src/main/java/com/tls/allergy/composite/UserAllergy.java
+++ BE/src/main/java/com/tls/allergy/composite/UserAllergy.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
