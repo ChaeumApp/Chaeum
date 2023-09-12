06101ccf9e5349fb1502bbf44af1937959b6c0bd
박은규 <parkeg1223@gmail.com>
2023-09-12 17:49:56 +0900

    feat: 알러지 관련 entity 구현

---
+++ BE/src/main/java/com/tls/allergy/composite/UserAllergy.java
@@ -0,0 +1,33 @@
+package com.tls.allergy.composite;
+
+import com.tls.allergy.single.Allergy;
+import com.tls.user.entity.User;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_allergy_tb")
+public class UserAllergy {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "algy_id")
+    private Allergy allergy;
+}
