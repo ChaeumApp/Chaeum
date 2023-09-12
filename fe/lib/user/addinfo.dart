78c6694e34c1083dc727f99dbd85865db90775c0
이지혁 <illu140411@gmail.com>
2023-09-12 11:41:05 +0900

    feat: 회원가입 정보 기입
    Former-commit-id: ab4323ac0d5ff4e42e307cc30ac99bf0768fdae1

--- fe/lib/user/addinfo.dart
+++ fe/lib/user/addinfo.dart
@@ -1,29 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AddInfo extends StatefulWidget {
-  const AddInfo({super.key});
+  AddInfo({super.key});
 
   @override
   State<AddInfo> createState() => _AddInfoState();
+  List<String> veganList = ['비건1', '비건2', '비건3'];
+  String selectedVegan = '비건1';
+  List<String> allergieList = ['없음', '있음'];
+  String selectedAllergie = '없음';
 }
 
 class _AddInfoState extends State<AddInfo> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('필수 추가 정보'),
-              Text('맞춤 정보 제공을 위해\n필수 추가 정보를 입력해주세요.'),
-            ],
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '필수 추가 정보',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                ),
+                Text('맞춤 정보 제공을 위해\n필수 추가 정보를 입력해주세요.'),
+              ],
+            ),
           ),
-          Text('생년월일'),
+          Text(
+            '생년월일 및 성별',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                    fit: FlexFit.tight,
+                    flex: 5,
+                    child: TextField(
+                      maxLength: 6,
+                      decoration: InputDecoration(
+                          isDense: true,
+                          counterText: '',
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 10.0),
+                          focusColor: Color(0xffA1CBA1)),
+                    )),
+                Flexible(fit: FlexFit.tight, flex: 2, child: Text('     ㅡ ')),
+                Flexible(
+                    fit: FlexFit.tight,
+                    flex: 1,
+                    child: TextField(
+                      maxLength: 1,
+                      decoration: InputDecoration(
+                          isDense: true,
+                          counterText: '',
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 10.0),
+                          focusColor: Color(0xffA1CBA1)),
+                    )),
+                Flexible(
+                    fit: FlexFit.tight,
+                    flex: 4,
+                    child: Text(
+                      ' * * * * * *',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                    ))
+              ],
+            ),
+          ),
           Row(
             children: [
               Flexible(
@@ -32,8 +88,26 @@ class _AddInfoState extends State<AddInfo> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('비건여부'),
-                    Text('선택창'),
+                    Text(
+                      '비건여부',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                    ),
+                    DropdownButton(
+                      alignment: Alignment.bottomRight,
+                      value: widget.selectedVegan,
+                      items: widget.veganList.map((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                      onChanged: (dynamic value) {
+                        setState(() {
+                          widget.selectedVegan = value;
+                        });
+                      },
+                    ),
                   ],
                 ),
               ),
@@ -43,14 +117,45 @@ class _AddInfoState extends State<AddInfo> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('알러지여부'),
-                    Text('선택창'),
+                    Text(
+                      '알러지여부',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                    ),
+                    DropdownButton(
+                      alignment: Alignment.bottomRight,
+                      value: widget.selectedAllergie,
+                      items: widget.allergieList.map((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                      onChanged: (dynamic value) {
+                        setState(() {
+                          widget.selectedAllergie = value;
+                        });
+                      },
+                    ),
                   ],
                 ),
               ),
             ],
           ),
-          Text('제출하기'),
+          SizedBox(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(Color(0xffA1CBA1))),
+                    child: Text('제출하기')),
+              ],
+            ),
+          ),
           Text('이전으로')
         ],
       ),
