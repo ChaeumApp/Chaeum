4ba4221049198ea182dd69e0e31a77bb6a1f132f
박은규 <parkeg1223@gmail.com>
2023-09-12 17:51:24 +0900

    style: unused import 구문 삭제

--- BE/src/main/java/com/tls/allergy/repository/AllergyIngredientRepository.java
+++ BE/src/main/java/com/tls/allergy/repository/AllergyIngredientRepository.java
@@ -1,6 +1,5 @@
 package com.tls.allergy.repository;
 
-import com.tls.Ingredient.entity.single.Ingredient;
 import com.tls.allergy.composite.AllergyIngredient;
 import java.util.List;
 import java.util.Optional;
