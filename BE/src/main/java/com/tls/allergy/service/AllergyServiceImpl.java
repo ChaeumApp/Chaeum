4375226e7eafbe7fbd94bfe73cf79104ad45a5e0
박은규 <parkeg1223@gmail.com>
2023-09-13 17:54:37 +0900

    feat: 알러지 샘플 코드 개발

---
+++ BE/src/main/java/com/tls/allergy/service/AllergyServiceImpl.java
@@ -0,0 +1,30 @@
+package com.tls.allergy.service;
+
+import com.tls.allergy.composite.UserAllergy;
+import com.tls.allergy.repository.AllergyRepository;
+import com.tls.allergy.repository.UserAllergyRepository;
+import com.tls.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AllergyServiceImpl implements AllergyService {
+
+    private final UserRepository userRepository;
+    private final AllergyRepository allergyRepository;
+    private final UserAllergyRepository userAllergyRepository;
+    @Override
+    public int addUserAllergy(String userEmail, int algyId) {
+        try {
+            // 기존에 있는지 확인
+            UserAllergy userAllergy = UserAllergy.builder()
+                .userId(userRepository.findByUserEmail(userEmail).orElse(null))
+                .algyId(allergyRepository.findByAlgyId(algyId).orElse(null))
+                .build();
+        } catch (Exception e) {
+            return 406;
+        }
+        return 200;
+    }
+}
