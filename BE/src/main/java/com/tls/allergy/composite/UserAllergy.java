23ef0644d69e4e72421487c951a35b3aa64cf086
박은규 <parkeg1223@gmail.com>
2023-09-13 16:18:00 +0900

    feat: UserAllergy 복합 키 설정

--- BE/src/main/java/com/tls/allergy/composite/UserAllergy.java
+++ BE/src/main/java/com/tls/allergy/composite/UserAllergy.java
@@ -1,10 +1,12 @@
 package com.tls.allergy.composite;
 
+import com.tls.allergy.id.UserAllergyId;
 import com.tls.allergy.single.Allergy;
 import com.tls.user.entity.User;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -19,14 +21,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user_allergy_tb")
+@IdClass(UserAllergyId.class)
 public class UserAllergy {
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
+    private User userId;
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "algy_id")
-    private Allergy allergy;
+    private Allergy algyId;
 }
