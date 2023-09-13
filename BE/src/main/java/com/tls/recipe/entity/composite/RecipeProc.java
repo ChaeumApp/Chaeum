61a8da8d9311f1a161e61f200281371b3bd960bb
박은규 <parkeg1223@gmail.com>
2023-09-13 16:17:49 +0900

    feat: RecipeProc 복합 키 설정

--- BE/src/main/java/com/tls/recipe/entity/composite/RecipeProc.java
+++ BE/src/main/java/com/tls/recipe/entity/composite/RecipeProc.java
@@ -1,10 +1,12 @@
 package com.tls.recipe.entity.composite;
 
 import com.tls.recipe.entity.single.Recipe;
+import com.tls.recipe.id.RecipeProcId;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -19,12 +21,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "recipe_process_tb")
-public class RecipeProcess {
+@IdClass(RecipeProcId.class)
+public class RecipeProc {
 
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_id")
-    private Recipe recipe;
+    private Recipe recipeId;
 
     @Id
     @Column(name = "recipe_proc_id")
