d271ddf773d4f3aef23e934575e88600d04578de
박은규 <parkeg1223@gmail.com>
2023-09-14 17:53:50 +0900

    style: unused import 구문 삭제
    Former-commit-id: 3b48f5de578b291e4df59b284a0948156f1589b4

--- BE/src/main/java/com/tls/category/service/CategoryService.java
+++ BE/src/main/java/com/tls/category/service/CategoryService.java
@@ -2,10 +2,9 @@ package com.tls.category.service;
 
 import com.tls.category.entity.Category;
 import java.util.List;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CategoryService {
+
     List<Category> getCategories();
 
     Category getCategory(int catId);
