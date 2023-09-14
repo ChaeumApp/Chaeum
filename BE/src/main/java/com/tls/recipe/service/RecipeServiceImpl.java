0f5bfaa287319c84dbf1c4840d0ecb22d40edce0
gunhoo <gunhoo2016@gmail.com>
2023-09-14 12:01:56 +0900

    feat: recipe 상세조회 완성
    - Recipe Entity 에 Thumbnail column 추가
    - Controller 추가
    - Repository에 findBy 추가
    - Service code 작성
    - schema 변경
    
    
    Former-commit-id: 357fdedff870f944f9febc66f6f90a2c368438e9

--- BE/src/main/java/com/tls/recipe/service/RecipeServiceImpl.java
+++ BE/src/main/java/com/tls/recipe/service/RecipeServiceImpl.java
@@ -1,14 +1,23 @@
 package com.tls.recipe.service;
 
+import com.tls.recipe.entity.single.Recipe;
+import com.tls.recipe.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class RecipeServiceImpl implements RecipeService{
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
 
     @Override
     public int updateRecipe() {
         return (int) (Math.random() % 2);
+    }
+
+    @Override
+    public Recipe viewRecipe(int recipeId) {
+        return recipeRepository.findByRecipeId(recipeId).orElse(null);
     }
 }
