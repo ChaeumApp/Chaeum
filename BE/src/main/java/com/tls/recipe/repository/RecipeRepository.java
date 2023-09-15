88f2838ca77b58397aa1d4880d9546a9dedb67a9
gunhoo <gunhoo2016@gmail.com>
2023-09-15 09:40:11 +0900

    feat: 소분류로 레시피 검색 기능 구현
    Former-commit-id: 1b434e2044bd1d47ac0f772dac04d36e954f0d54

--- BE/src/main/java/com/tls/recipe/repository/RecipeRepository.java
+++ BE/src/main/java/com/tls/recipe/repository/RecipeRepository.java
@@ -1,12 +1,15 @@
 package com.tls.recipe.repository;
 
 import com.tls.recipe.entity.single.Recipe;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface RecipeRepository extends Repository<Recipe, String> {
 
     Optional<Recipe> findByRecipeId(int recipeId);
+
+    List<Recipe> findByRecipeNameLike(String recipeName);
 
     void save(Recipe recipe);
 }
