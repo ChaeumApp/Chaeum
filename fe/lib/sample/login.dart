85745aacb77e56fec1315a1f29499671d535699e
이지혁 <illu140411@gmail.com>
2023-09-15 10:39:25 +0900

    feat: 로그인 추가
    Former-commit-id: b481189e04de39cc328dfb989fe4215f9d348d47

--- fe/lib/sample/login.dart
+++ fe/lib/sample/login.dart
@@ -12,14 +12,85 @@ class _LoginSampleState extends State<LoginSample> {
   Dio dio = Dio(BaseOptions(
     baseUrl: 'http://10.0.2.2:8080',
     // 토큰 넣는거 필요하면
-    // headers: {'Authorization': 'Bearer Token'},
+    headers: {'Authorization': 'Bearer Token'},
   ));
 
   //로그인
   Future<void> login() async {
     try {
       Response response = await dio.post('/user/signin',
-          data: {'userEmail': 'ssafy', 'userPwd': 'pass123!'});
+          data: {'userEmail': 'ww@ssafy.com', 'userPwd': 'pass123!'});
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> signup() async {
+    try {
+      Response response = await dio.post('/user/signup', data: {
+        "userActivated": true,
+        "userBirthday": "string",
+        "userEmail": "string",
+        "userGender": "string",
+        "userId": 0,
+        "userPwd": "string",
+        "veganId": 0
+      });
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> signout() async {
+    try {
+      Response response = await dio.post('/user/signout', data: {});
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> finduserpwd() async {
+    try {
+      Response response = await dio.post('/user/find/pwd', data: {
+        "userEmail": "string",
+        "userBirthday": "string",
+      });
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> deleteuser() async {
+    try {
+      Response response = await dio.delete('/user', data: {
+        "userEmail": "string",
+      });
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> updateuser() async {
+    try {
+      Response response = await dio.put('/user', data: {
+        "userEmail": "string",
+      });
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> getuserinfo() async {
+    try {
+      Response response = await dio.get('/user/mypage', data: {
+        "userEmail": "string",
+      });
       print(response.data);
     } catch (e) {
       print(e);
