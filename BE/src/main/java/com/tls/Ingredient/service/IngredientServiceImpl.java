e99cc34649e55f412b95cd8930675ff1345d2385
박은규 <parkeg1223@gmail.com>
2023-09-15 09:40:57 +0900

    feat: 소분류 관련 샘플 코드 구현
    Former-commit-id: c2407a362302b8da7e34af3d1843bf7aa68f8fa9

--- BE/src/main/java/com/tls/Ingredient/service/IngredientServiceImpl.java
+++ BE/src/main/java/com/tls/Ingredient/service/IngredientServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 public class IngredientServiceImpl implements IngredientService {
 
     @Override
-    public IngredientDto getIngredients() {
+    public List<IngredientDto> getIngredients() {
         return null;
     }
 
