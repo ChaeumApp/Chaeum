f695f7ae2785b8cebfc0212dd9937d76b55ad4e5
오연주 <siunaus224@gmail.com>
2023-09-15 09:18:58 +0900

    feat: 메인, 상세 페이지 샘플코드 완료
    Former-commit-id: c08b23338933465b287a7b572272601c40911358

--- fe/lib/sample/main.dart
+++ fe/lib/sample/main.dart
@@ -18,6 +18,22 @@ class _MainSampleState extends State<MainSample> {
   ));
 
 
+  // 대분류 목록 받아오기
+  var category = [];
+
+  Future<void> getCategory(email) async {
+    try {
+      Response response = await dio.get('/category');
+      setState(() {
+        category = response.data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
+
   // 베스트 목록 받아오기
   var bestList = [];
 
@@ -49,6 +65,30 @@ class _MainSampleState extends State<MainSample> {
       print(e);
     }
   }
+
+  // 상품 선택
+  Future<void> clickItem(email, itemId) async {
+    try {
+      Response response = await dio.post('/item/selected',
+          data: {'userEmail': email, 'itemId': itemId});
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // 소분류 선택
+  Future<void> clickCategory(email, ingrId) async {
+    try {
+      Response response = await dio.post('/ingr/selected',
+          data: {'userEmail': email, 'ingrId': ingrId});
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
 
 
   @override
