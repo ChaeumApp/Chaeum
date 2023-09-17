8271197edd9db0b394c1107ead7d795cd50e9adb
박건후 <gunhoo2016@gmail.com>
2023-09-17 22:08:11 +0900

    feat: update recipe by excel
    - recipe.xlsx file 읽어서 DB에 저장
    
    
    Former-commit-id: e60ca085b70124ff49d38c4f4a8a29139ac2c055

--- BE/src/main/java/com/tls/recipe/service/RecipeServiceImpl.java
+++ BE/src/main/java/com/tls/recipe/service/RecipeServiceImpl.java
@@ -1,9 +1,9 @@
 package com.tls.recipe.service;
 
+import com.tls.config.ReadExcel;
 import com.tls.recipe.entity.composite.UserRecipe;
 import com.tls.recipe.entity.composite.UserRecipeLog;
 import com.tls.recipe.entity.single.Recipe;
-import com.tls.recipe.id.UserRecipeId;
 import com.tls.recipe.repository.RecipeRepository;
 import com.tls.recipe.repository.UserRecipeLogRepository;
 import com.tls.recipe.repository.UserRecipeRepository;
@@ -12,8 +12,10 @@ import com.tls.user.entity.User;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RecipeServiceImpl implements RecipeService {
@@ -25,7 +27,24 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public int updateRecipe() {
-        return (int) (Math.random() % 2);
+        try {
+            List<String[]> recipes = new ReadExcel().readExcel(
+                "D://ssafy//2학기//채움//S09P22C204//RecipeCrawling//", "recipe.xlsx");
+            recipeRepository.deleteAllInBatch();
+            for (String[] oneRecipe : recipes) {
+                Recipe recipe = Recipe.builder()
+                    .recipeName(oneRecipe[0])
+                    .recipeLink(oneRecipe[4])
+                    .recipeThumbnail(oneRecipe[3])
+                    .build();
+                recipeRepository.save(recipe);
+            }
+            log.info("{} recipes are updated", recipes.size());
+            return 1;
+        } catch (Exception e) {
+            log.info("error occurred updating recipes");
+            return -1;
+        }
     }
 
     @Override
@@ -62,7 +81,8 @@ public class RecipeServiceImpl implements RecipeService {
         try {
             User user = userRepository.findByUserEmail(userEmail).orElseThrow();
             Recipe recipe = recipeRepository.findByRecipeId(recipeId).orElseThrow();
-            if (userRecipeRepository.findByUserIdAndRecipeId(user, recipe).isEmpty()) { // 즐겨찾기에 존재한다면
+            if (userRecipeRepository.findByUserIdAndRecipeId(user, recipe)
+                .isEmpty()) { // 즐겨찾기에 존재한다면
                 UserRecipe userRecipe = UserRecipe.builder()
                     .userId(userRepository.findByUserEmail(userEmail).orElseThrow())
                     .recipeId(recipeRepository.findByRecipeId(recipeId).orElseThrow()).build();
