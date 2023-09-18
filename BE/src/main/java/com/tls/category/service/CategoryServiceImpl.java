9e05294d0b4340e49366abe797cf18ac785c74a2
gunhoo <gunhoo2016@gmail.com>
2023-09-18 16:49:26 +0900

    feat: 대분류 <-> 중분류 묶어서 조회 기능 추가
    Former-commit-id: 8a051fdd39430f0d0b2bb884a3388f5eb7d3eed3

--- BE/src/main/java/com/tls/category/service/CategoryServiceImpl.java
+++ BE/src/main/java/com/tls/category/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.tls.category.service;
 
+import com.tls.category.dto.CategoryWithSubCategoryDto;
 import com.tls.category.entity.Category;
 import com.tls.category.repository.CategoryRepository;
+import com.tls.category.repository.SubCategoryRepository;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final SubCategoryRepository subCategoryRepository;
 
     @Override
     public List<Category> getCategories() {
@@ -25,6 +29,47 @@ public class CategoryServiceImpl implements CategoryService {
     public Category getCategory(int catId) {
         try {
             return categoryRepository.findByCatId(catId).orElse(null);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<CategoryWithSubCategoryDto> getCategoryWithSubCategory() {
+        try {
+            List<CategoryWithSubCategoryDto> results = new ArrayList<>();
+            getCategories().forEach(category -> {
+                System.out.println("---");
+                System.out.println(category.getCatName());
+                CategoryWithSubCategoryDto categoryWithSubCategoryDto = CategoryWithSubCategoryDto.builder()
+                    .catId(category.getCatId())
+                    .catName(category.getCatName())
+                    .subCategoryDtoList(subCategoryRepository.findByCatId(category))
+                    .build();
+                System.out.println(categoryWithSubCategoryDto.getCatName());
+                results.add(categoryWithSubCategoryDto);
+            });
+
+//            List<CategoryWithSubCategoryDto> results = new ArrayList<>();
+//            for (Category category : categories) {
+//                Optional<SubCategory> subCategoryDtoList = subCategoryRepository.findBySubCatId(
+//                    category.getCatId());
+//                List<SubCategoryDto> subCategoryDtos = new ArrayList<>();
+//                subCategoryDtoList.ifPresent(subCategory -> {
+//                    SubCategoryDto subCategoryDto = SubCategoryDto.builder()
+//                        .subcatId(subCategory.getSubCatId())
+//                        .subcatName(subCategory.getSubCatName())
+//                        .build();
+//                    subCategoryDtos.add(subCategoryDto);
+//                });
+//                CategoryWithSubCategoryDto categoryWithSubCategoryDto = CategoryWithSubCategoryDto.builder()
+//                    .catName(category.getCatName())
+//                    .catId(category.getCatId())
+//                    .subCategoryDtoList(subCategoryDtos)
+//                    .build();
+//                results.add(categoryWithSubCategoryDto);
+//            }
+            return results;
         } catch (Exception e) {
             return null;
         }
