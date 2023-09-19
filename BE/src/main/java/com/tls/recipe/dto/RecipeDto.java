f6667348a1b597af8f8babe716276f9e492b1237
gunhoo <gunhoo2016@gmail.com>
2023-09-19 16:05:07 +0900

    fix: recipe detail view 수정
    - 레시피 순서 반환 안해주는 문제 수정
    
    
    Former-commit-id: b6971651aba72770359392912b90b319f533c973

---
+++ BE/src/main/java/com/tls/recipe/dto/RecipeDto.java
@@ -0,0 +1,16 @@
+package com.tls.recipe.dto;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class RecipeDto {
+
+    String recipeName;
+    String recipeThumbnail;
+    String recipeLink;
+    List<String> recipeProcess;
+    List<String> recipeIngredients;
+}
--- BE/src/main/java/com/tls/recipe/repository/RecipeProcRepository.java
+++ BE/src/main/java/com/tls/recipe/repository/RecipeProcRepository.java
@@ -1,8 +1,11 @@
 package com.tls.recipe.repository;
 
 import com.tls.recipe.entity.composite.RecipeProc;
+import com.tls.recipe.entity.single.Recipe;
 import com.tls.recipe.id.RecipeProcId;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeProcRepository extends JpaRepository<RecipeProc, RecipeProcId> {
+    List<RecipeProc> findByRecipeId(Recipe recipe);
 }
