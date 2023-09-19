5a350e2ae64106f7377003fe4d19d87c5fd1f8c0
오연주 <siunaus224@gmail.com>
2023-09-19 14:34:28 +0900

    feat: 레시피 메인 페이지 생성
    Former-commit-id: 7dd9019f14215219b0d67d047e8738b5c67c1a2e

--- fe/lib/main/maincategory.dart
+++ fe/lib/main/maincategory.dart
@@ -1,3 +1,4 @@
+import 'package:fe/category/catepage.dart';
 import 'package:flutter/material.dart';
 
 class MainCategory extends StatelessWidget {
@@ -9,31 +10,40 @@ class MainCategory extends StatelessWidget {
       {'name' : '과일', 'image' : 'orange'},
       {'name' : '채소', 'image' : 'cabbage'},
       {'name' : '곡류/견과', 'image' : 'rice'},
-      {'name' : '정육/계란', 'image' : 'meat'},
-      {'name' : '수산', 'image' : 'seafood'},
+      {'name' : '정육/달걀', 'image' : 'meat'},
+      {'name' : '수산물', 'image' : 'seafood'},
       {'name' : '유제품', 'image' : 'milk'},
       {'name' : '김치', 'image' : 'kimchi'},
-      {'name' : '면', 'image' : 'noodle'},
+      {'name' : '면/파스타', 'image' : 'noodle'},
       {'name' : '통조림', 'image' : 'canned'},
-      {'name' : '조미료', 'image' : 'salt'},
+      {'name' : '가루/조미료', 'image' : 'salt'},
       {'name' : '오일/소스', 'image' : 'oil'},
-      {'name' : '베이커리', 'image' : 'bakery'}
+      {'name' : '빵/잼', 'image' : 'bakery'}
     ];
 
     return SliverGrid(delegate: SliverChildBuilderDelegate(
-        (c, i)=> Center(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(13, 0, 13, 0),
-                width: 60,
-                  height: 50,
-                  child: Image.asset('assets/images/main/${category[i]['image']}.png',)),
-              Text('${category[i]['name']}',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                ),)
-            ],
+        (c, i)=> GestureDetector(
+          onTap: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        CatePage()));
+          },
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(13, 0, 13, 0),
+                  width: 60,
+                    height: 50,
+                    child: Image.asset('assets/images/main/${category[i]['image']}.png',)),
+                Text('${category[i]['name']}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),)
+              ],
+            ),
           ),
         ),
     childCount: 12),
