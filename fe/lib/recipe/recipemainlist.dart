e131fb0dae0628a885cacb9e03cc763ac398f6f8
hwnagsua <tsmich926@naver.com>
2023-09-19 17:43:50 +0900

    feat:레시피디테일 디버깅
    Former-commit-id: 2159fb6bfecdd8ae268c09a3fa7101ef03dc95d3

--- fe/lib/recipe/recipemainlist.dart
+++ fe/lib/recipe/recipemainlist.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:fe/search/detail/recipedetail.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -26,120 +27,128 @@ class _RecipeMainListState extends State<RecipeMainList> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getCategory(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData == false) {
-          return Shimmer.fromColors(
-            baseColor: Colors.grey.shade300,
-            highlightColor: Colors.grey.shade100,
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 2,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Shimmer.fromColors(
-                      baseColor: Colors.grey.shade300,
-                      highlightColor: Colors.grey.shade100,
-                      child: Container(
-                        height: 220,
-                        width: double.infinity,
-                        color: Colors.white,
+        future: getCategory(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData == false) {
+            return Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 2,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Shimmer.fromColors(
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        child: Container(
+                          height: 220,
+                          width: double.infinity,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(20, 15, 20, 30),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Container(
-                              height: 40,
-                              width: 40,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(width: 15),
-                          Expanded(
-                            child: Shimmer.fromColors(
-                              baseColor: Colors.grey.shade300,
-                              highlightColor: Colors.grey.shade100,
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 15, 20, 30),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
                               child: Container(
-                                height: 20,
+                                height: 40,
+                                width: 40,
                                 color: Colors.white,
                               ),
                             ),
-                          ),
-                        ],
+                            SizedBox(width: 15),
+                            Expanded(
+                              child: Shimmer.fromColors(
+                                baseColor: Colors.grey.shade300,
+                                highlightColor: Colors.grey.shade100,
+                                child: Container(
+                                  height: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                );
-              },
-            ),
-          );
-        }
-        //error가 발생하게 될 경우 반환하게 되는 부분
-        else if (snapshot.hasError) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Error: ${snapshot.error}',
-              style: TextStyle(fontSize: 15),
-            ),
-          );
-        }
-        // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 부분
-        else {
-          return Expanded(
-                child: ListView.builder(
+                    ],
+                  );
+                },
+              ),
+            );
+          }
+          //error가 발생하게 될 경우 반환하게 되는 부분
+          else if (snapshot.hasError) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Error: ${snapshot.error}',
+                style: TextStyle(fontSize: 15),
+              ),
+            );
+          }
+          // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 부분
+          else {
+            return Expanded(
+              child: ListView.builder(
                   shrinkWrap: true,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return GestureDetector(
-                        onTap: (){
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (context) =>
-                          //             ));
-                        },
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(snapshot.data[index]['recipeThumbnail'],
-                            height: 220,
-                            width: double.infinity,
-                            fit: BoxFit.fill),
-                            Container(
-                                margin: EdgeInsets.fromLTRB(20, 15, 20, 30),
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(50),
-                                      child: Image.asset('assets/images/recipe/recipe.png',
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Recipedetail(
+                                    recipeId: snapshot.data[index]
+                                        ['recipeId'])));
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(snapshot.data[index]['recipeThumbnail'],
+                              height: 220,
+                              width: double.infinity,
+                              fit: BoxFit.fill),
+                          Container(
+                              margin: EdgeInsets.fromLTRB(20, 15, 20, 30),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: Image.asset(
+                                      'assets/images/recipe/recipe.png',
                                       height: 40,
-                                      width: 40,),
+                                      width: 40,
                                     ),
-                                    SizedBox(width: 15,),
-                                    Expanded(
-                                      child: Text('${snapshot.data[index]['recipeName']}',
+                                  ),
+                                  SizedBox(
+                                    width: 15,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      '${snapshot.data[index]['recipeName']}',
                                       style: TextStyle(
                                         fontSize: 18,
                                         fontWeight: FontWeight.w600,
-                                      ),softWrap: true,),
+                                      ),
+                                      softWrap: true,
                                     ),
-                                  ],
-                                ))
-                          ],
-                        ),
-                      );
-                    }),
-              );
-        }
-      });
+                                  ),
+                                ],
+                              ))
+                        ],
+                      ),
+                    );
+                  }),
+            );
+          }
+        });
   }
 }
