85bed0b2add257cdb3445b14a5316b98df82c5cc
hwnagsua <tsmich926@naver.com>
2023-09-20 10:47:01 +0900

    feat:converturl변경
    Former-commit-id: df731772a98bb4c2638f77199d55d87b0fb690cd

--- fe/lib/search/detail/youtube.dart
+++ fe/lib/search/detail/youtube.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
+// class Recipeyoutube extends StatefulWidget {
+//   Recipeyoutube({super.key, this.recipeLink});
+//   final recipeLink;
+//   @override
+//   State<Recipeyoutube> createState() => _RecipeyoutubeState();
+// }
+
+// class _RecipeyoutubeState extends State<Recipeyoutube> {
+//   // static String youtubeId = 'o5uTQF7b9Og';
+
+//   final YoutubePlayerController _con = YoutubePlayerController(
+//       initialVideoId: YoutubePlayer.convertUrlToId(
+//               widget.recipeLink)
+//           .toString(),
+//       flags: const YoutubePlayerFlags(
+//         autoPlay: true,
+//         mute: false,
+//       ));
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: YoutubePlayer(
+//         controller: _con,
+//       ),
+//     );
+//   }
+// }
+
 class Recipeyoutube extends StatefulWidget {
   Recipeyoutube({super.key, this.recipeLink});
   final recipeLink;
+
   @override
   State<Recipeyoutube> createState() => _RecipeyoutubeState();
 }
 
 class _RecipeyoutubeState extends State<Recipeyoutube> {
-  // static String youtubeId = 'o5uTQF7b9Og';
+  late final YoutubePlayerController _con;
 
-  final YoutubePlayerController _con = YoutubePlayerController(
-      initialVideoId: YoutubePlayer.convertUrlToId(
-              'https://www.youtube.com/watch?v=uSljGJGSl6w')
-          .toString(),
+  @override
+  void initState() {
+    super.initState();
+
+    _con = YoutubePlayerController(
+      initialVideoId:
+          YoutubePlayer.convertUrlToId(widget.recipeLink).toString(),
       flags: const YoutubePlayerFlags(
         autoPlay: true,
         mute: false,
-      ));
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
