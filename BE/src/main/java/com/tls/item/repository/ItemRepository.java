59889ae8080abcd1c1a4b5a63d341b38173ef91a
gunhoo <gunhoo2016@gmail.com>
2023-09-21 17:49:25 +0900

    feat: item(상품) API 작성
    Former-commit-id: 66ea6537398eb80e4969b64f8cfae5b48645da82

--- BE/src/main/java/com/tls/item/repository/ItemRepository.java
+++ BE/src/main/java/com/tls/item/repository/ItemRepository.java
@@ -1,8 +1,15 @@
 package com.tls.item.repository;
 
+import com.tls.ingredient.entity.single.Ingredient;
 import com.tls.item.entity.single.Item;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface ItemRepository extends Repository<Item, String> {
+
+    Optional<List<Item>> findByIngredient(Ingredient ingredient);
+
+    Optional<Item> findByItemId(long itemId);
     void save(Item item);
 }
---
+++ BE/src/main/java/com/tls/item/repository/UserItemLogRepository.java
@@ -0,0 +1,8 @@
+package com.tls.item.repository;
+
+import com.tls.item.entity.composite.UserItemLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserItemLogRepository extends JpaRepository<UserItemLog, Long> {
+
+}
