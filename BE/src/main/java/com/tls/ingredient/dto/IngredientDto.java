346de068d028a0a9fe0ca7e02de97362068d61c0
박은규 <parkeg1223@gmail.com>
2023-09-21 09:59:00 +0900

    refactor: 패키지명 수정
    Former-commit-id: 989169f3e9d057afb66c7fac7586a390b7901ac5

---
+++ BE/src/main/java/com/tls/ingredient/dto/IngredientDto.java
@@ -0,0 +1,11 @@
+package com.tls.ingredient.dto;
+
+public class IngredientDto {
+
+    int ingrId;
+    String ingrName;
+    String subCategory;
+    String category;
+    
+    // 가격 정보
+}
---
+++ BE/src/main/java/com/tls/ingredient/service/IngredientService.java
@@ -0,0 +1,23 @@
+package com.tls.ingredient.service;
+
+import com.tls.ingredient.dto.IngredientDto;
+import com.tls.ingredient.vo.IngredientVO;
+import com.tls.ingredient.vo.UserIngrVO;
+import java.util.List;
+
+public interface IngredientService {
+
+    List<IngredientDto> getIngredients();
+
+    List<IngredientDto> getIngredients(int catId, int subcatId);
+
+    List<IngredientDto> getBestIngredients();
+
+    IngredientDto getIngredient(int ingrId);
+
+    int selectIngredient(UserIngrVO userIngrVO);
+
+    int dislikeIngredient(IngredientVO ingredientVO);
+
+    int favoriteIngredient(String userEmail, int ingrId);
+}
