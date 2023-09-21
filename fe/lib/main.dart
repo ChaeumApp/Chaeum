e459f6729ea06f4d4ae46eb1581e0a474b279265
Yeoni <siunaus224@gmail.com>
2023-09-21 20:31:09 +0900

    feat: 뒤로가기 두번 눌러서 종료, 검색창 창 띄우기 변경
    Former-commit-id: c028c49c2d9a64fe59b720c267ea1b1b4a2a6808

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -11,6 +11,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import './repeat/bottom.dart';
 //카카오로그인
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
@@ -96,28 +97,51 @@ class _MainState extends State<Main> {
     print('시작2');
   }
 
+  DateTime? currentBackPressTime;
+  Future<bool> onWillPop(){
+    DateTime now = DateTime.now();
+    if(currentBackPressTime == null || now.difference(currentBackPressTime!) > Duration(seconds: 2))
+    {
+      currentBackPressTime = now;
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Color(0xff4C8C4C),
+            content: Text('한번 더 뒤로가기를 누를 시 종료됩니다'),
+            duration: Duration(seconds: 2),
+          ),
+      );
+      return Future.value(false);
+    }
+    return Future.value(true);
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        initialIndex: 2,
-        length: 5,
-        child: Scaffold(
-          body: SafeArea(
-            child: TabBarView(
-              children: [
-                CategoryMain(),
-                RecipeMain(),
-                Mainb(),
-                SearchMain(),
-                userInfo == null
-                    ? LogIn(storage: storage)
-                    : MyPage(storage: storage)
-                // FavoriteMore(),
-              ],
+    return WillPopScope(
+      onWillPop: onWillPop,
+      child: DefaultTabController(
+          initialIndex: 2,
+          length: 5,
+          child: Scaffold(
+            body: SafeArea(
+              child: TabBarView(
+                children: [
+                  CategoryMain(),
+                  RecipeMain(),
+                  Mainb(),
+                  SearchMain(),
+                  userInfo == null
+                      ? LogIn(storage: storage)
+                      : MyPage(storage: storage)
+                  // FavoriteMore(),
+                ],
+              ),
             ),
-          ),
-          extendBodyBehindAppBar: true,
-          bottomNavigationBar: Bottom(),
-        ));
+            extendBodyBehindAppBar: true,
+            bottomNavigationBar: Bottom(),
+          )),
+    );
   }
 }
+
