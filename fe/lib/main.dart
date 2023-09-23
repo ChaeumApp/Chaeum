1c384c6d3c6753e28e0e825d5a1e63abb831e4cf
Lee Ji Hyeok <illu140411@gmail.com>
2023-09-23 18:30:24 +0900

    refactor: 회원가입 수정
    Former-commit-id: ffe8c958d03d9598a461da1bc33d00c3626bf5a5

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -86,26 +86,29 @@ class _MainState extends State<Main> {
       foregroundMessage(message);
     });
     initializeNotification(context);
+
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await _asyncMethod();
       setState(() {});
     });
-    super.initState();
   }
 
   _asyncMethod() async {
     //read 함수를 통하여 key값에 맞는 정보를 불러오게 됩니다. 이때 불러오는 결과의 타입은 String 타입임을 기억해야 합니다.
     //(데이터가 없을때는 null을 반환을 합니다.)
-
+    var key = await KakaoSdk.origin;
+    print('kakaokey$key');
     userToken = await storage.read(key: "login");
     print(userToken);
-    userToken == null
-        ? () async {
-            await context.read<UserStore>().changeAccessToken(
-                userToken.toString().split(" ")[1].toString());
-          }
-        : null;
+    if (userToken != null) {
+      await context
+          .read<UserStore>()
+          .changeAccessToken(userToken.toString().split(" ")[1].toString());
+    }
 
+    final storetoken = context.read<UserStore>().accessToken;
+    print('이닛 다시 하나요?');
+    print(storetoken);
     final devicetoken = await getMyDeviceToken();
     await context.read<UserStore>().savedevicetoken(devicetoken.toString());
   }
