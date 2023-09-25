6f04a7baa73edcbc038295d121b60c8f5e534bdf
오연주 <siunaus224@gmail.com>
2023-09-25 14:54:56 +0900

    feat: 상세페이지 상품 불러오기
    Former-commit-id: d09b73758775a19af91fba9268d2f9425975277d

--- fe/lib/main/maincategory.dart
+++ fe/lib/main/maincategory.dart
@@ -8,18 +8,18 @@ class MainCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var category = [
-      {'catName' : '과일', 'image' : 'orange', 'catId' : 1, 'subCatId' : null},
-      {'catName' : '채소', 'image' : 'cabbage', 'catId' : 2, 'subCatId' : null},
-      {'catName' : '곡류/견과', 'image' : 'rice', 'catId' : 3, 'subCatId' : null},
+      {'catName' : '과일', 'image' : 'orange', 'catId' : 1, 'subCatId' : 0},
+      {'catName' : '채소', 'image' : 'cabbage', 'catId' : 2, 'subCatId' : 0},
+      {'catName' : '곡류/견과', 'image' : 'rice', 'catId' : 3, 'subCatId' : 0},
       {'catName' : '정육/달걀', 'image' : 'meat', 'catId' : 4, 'subCatId' : 0},
-      {'catName' : '수산물', 'image' : 'seafood', 'catId' : 5, 'subCatId' : null},
-      {'catName' : '유제품', 'image' : 'milk', 'catId' : 6, 'subCatId' : null},
-      {'catName' : '김치', 'image' : 'kimchi', 'catId' : 7, 'subCatId' : null},
-      {'catName' : '면/파스타', 'image' : 'noodle', 'catId' : 8, 'subCatId' : null},
-      {'catName' : '통조림', 'image' : 'canned', 'catId' : 9, 'subCatId' : null},
-      {'catName' : '가루/조미료', 'image' : 'salt', 'catId' : 10, 'subCatId' : null},
-      {'catName' : '오일/소스', 'image' : 'oil', 'catId' : 11, 'subCatId' : null},
-      {'catName' : '빵/잼', 'image' : 'bakery', 'catId' : 12, 'subCatId' : null}
+      {'catName' : '수산물', 'image' : 'seafood', 'catId' : 5, 'subCatId' : 0},
+      {'catName' : '유제품', 'image' : 'milk', 'catId' : 6, 'subCatId' : 0},
+      {'catName' : '김치', 'image' : 'kimchi', 'catId' : 7, 'subCatId' : 0},
+      {'catName' : '면/파스타', 'image' : 'noodle', 'catId' : 8, 'subCatId' : 0},
+      {'catName' : '통조림', 'image' : 'canned', 'catId' : 9, 'subCatId' : 0},
+      {'catName' : '가루/조미료', 'image' : 'salt', 'catId' : 10, 'subCatId' : 0},
+      {'catName' : '오일/소스', 'image' : 'oil', 'catId' : 11, 'subCatId' : 0},
+      {'catName' : '빵/잼', 'image' : 'bakery', 'catId' : 12, 'subCatId' : 0}
     ];
 
     return SliverGrid(delegate: SliverChildBuilderDelegate(
