bca1b462964464ce94dc90b9979a0c7241274963
박건후 <gunhoo2016@gmail.com>
2023-09-26 22:50:02 +0900

    feat: 소분류 관심없음 클릭시 선호도 점수 반영
    Former-commit-id: a051c125768e0eb7ab0114e92d01faf15c9083b9

--- BE/src/main/java/com/tls/ingredient/controller/IngredientController.java
+++ BE/src/main/java/com/tls/ingredient/controller/IngredientController.java
@@ -155,10 +155,10 @@ public class IngredientController {
     @PostMapping("/dislike")
     @Operation(summary = "소분류 관심없음 반영 메서드", description = "사용자가 특정 소분류를 관심없음 설정한 내용을 저장합니다.", tags = "소분류 API")
     public ResponseEntity<?> dislikeIngredient(@RequestHeader("Authorization")String tokenWithPrefix, @RequestBody IngredientVO ingredientVO) {
-        log.info("dislikeIngredient call :: ");
         try{
             Authentication authentication = jwtTokenProvider
                 .getAuthentication(tokenWithPrefix.substring(7));
+            log.info("dislikeIngredient call :: {} : {}", authentication.getName(), ingredientVO.getIngrId());
             int n = ingredientService.dislikeIngredient(authentication.getName(), ingredientVO);
             if (n == 1) {
                 return new ResponseEntity<>("success", HttpStatus.OK);
