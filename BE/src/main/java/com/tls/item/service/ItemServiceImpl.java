471cb2b71f129bec08bf6c344553fb5b539fd4b2
박건후 <gunhoo2016@gmail.com>
2023-09-26 22:42:33 +0900

    feat: 상품 클릭시 소분류 선호도 점수 반영
    Former-commit-id: 4b4b37bfef530880828b8aa0d09b54f3a4437d22

--- BE/src/main/java/com/tls/item/service/ItemServiceImpl.java
+++ BE/src/main/java/com/tls/item/service/ItemServiceImpl.java
@@ -1,18 +1,24 @@
 package com.tls.item.service;
 
+import com.tls.ingredient.entity.composite.IngredientPreference;
+import com.tls.ingredient.entity.single.Ingredient;
 import com.tls.ingredient.repository.IngrRepository;
+import com.tls.ingredient.repository.IngredientPreferenceRepository;
 import com.tls.item.converter.ItemConverter;
 import com.tls.item.dto.ItemDto;
 import com.tls.item.entity.composite.ItemPurchaseLog;
 import com.tls.item.entity.composite.UserItemLog;
+import com.tls.item.entity.single.Item;
 import com.tls.item.repository.ItemPurchaseLogRepository;
 import com.tls.item.repository.ItemRepository;
 import com.tls.item.repository.UserItemLogRepository;
 import com.tls.item.vo.ItemVO;
+import com.tls.user.entity.User;
 import com.tls.user.repository.UserRepository;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +31,7 @@ public class ItemServiceImpl implements ItemService {
     private final UserRepository userRepository;
     private final UserItemLogRepository userItemLogRepository;
     private final ItemPurchaseLogRepository itemPurchaseLogRepository;
+    private final IngredientPreferenceRepository ingredientPreferenceRepository;
     private final ItemConverter itemConverter = new ItemConverter();
 
     @Override
@@ -70,11 +77,28 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public int selectItem(String userEmail, ItemVO itemVO) {
         try {
+            User user = userRepository.findByUserEmail(userEmail).orElseThrow();
+            Item item = itemRepository.findByItemId(itemVO.getItemId()).orElseThrow();
             UserItemLog userItemLog = UserItemLog.builder()
-                .userId(userRepository.findByUserEmail(userEmail).orElseThrow())
-                .itemId(itemRepository.findByItemId(itemVO.getItemId()).orElseThrow())
+                .userId(user)
+                .itemId(item)
                 .build();
             userItemLogRepository.save(userItemLog);
+
+            IngredientPreference ingredientPreference = ingredientPreferenceRepository
+                .findByUserAndIngredient(user, item.getIngredient()).orElseThrow();
+            ingredientPreference.updatePrefRating(ingredientPreference.getPrefRating() + 10);
+
+            return 1;
+        } catch (NoSuchElementException e) { // 선호도 점수가 없을 경우 새로 만든다.
+            User user = userRepository.findByUserEmail(userEmail).orElseThrow();
+            Ingredient ingredient = itemRepository.findByItemId(itemVO.getItemId()).orElseThrow().getIngredient();
+            IngredientPreference ingredientPreference = IngredientPreference.builder()
+                .prefRating(15)
+                .ingredient(ingredient)
+                .user(user)
+                .build();
+            ingredientPreferenceRepository.save(ingredientPreference);
             return 1;
         } catch (Exception e) {
             return -1;
