6b8706f7fc71ddec864f745c2cfcbf8386bfd1ae
parkeg1223 <parkeg1223@gmail.com>
2023-09-29 23:51:59 +0900

    fix: 기존 crwaling scheduler 복구
    Former-commit-id: 2e96b1bbe1a335dedcc9b5b35aab5b68ae8e9751

---
+++ djangoServer/crawling/crawl_sche.py
@@ -0,0 +1,29 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import register_events, DjangoJobStore
+from .crawl_item import main
+from apscheduler.triggers.cron import CronTrigger
+from django.conf import settings
+import logging
+
+logger = logging.getLogger()
+
+def start():
+    scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
+    scheduler.add_jobstore(DjangoJobStore(), 'default')
+
+    scheduler.add_job(
+        main,
+        trigger=CronTrigger(hour="1", minute="40"),
+        id = "main",
+        max_instances=1,
+        replace_existing=True
+    )
+    
+    register_events(scheduler)
+
+    try:
+        logger.info("Starting scheduler...")
+        scheduler.start()
+    except KeyboardInterrupt:
+        logger.info("Stopping scheduler...")
+        scheduler.shutdown()
