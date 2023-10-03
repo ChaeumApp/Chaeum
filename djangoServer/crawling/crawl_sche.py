ed09adf3c04be1b46c2c0a8537f81e0f12e36d5c
parkeg1223 <parkeg1223@gmail.com>
2023-10-03 23:38:06 +0900

    feat: 가격 하락 소분류 알림 서비스 샘플 코드 구현
    Former-commit-id: 788027ad7328c3cb1f444f41efff784bcc7e3966 [formerly 349082d58a02bed777036d1ceb4ea40743237175]
    Former-commit-id: 5529dcc48eb93e1ed041cfa5b16425666100ed42

--- djangoServer/crawling/crawl_sche.py
+++ djangoServer/crawling/crawl_sche.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import register_events, DjangoJobStore
 from .crawl_item import main
+from .noti_price import IngredientPriceManager
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
 import logging
@@ -15,6 +16,14 @@ def start():
         main,
         trigger=CronTrigger(hour="2", minute="58"),
         id = "main",
+        max_instances=1,
+        replace_existing=True
+    )
+    
+    scheduler.add_job(
+        IngredientPriceManager.notify_pricedrop,
+        trigger=CronTrigger(hour="22", minute="22"),
+        id = "notiPrice",
         max_instances=1,
         replace_existing=True
     )
