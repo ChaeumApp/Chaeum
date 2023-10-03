dbe5789d4b73282ed27677c5a02cf20928ad0e29
박건후 <gunhoo2016@gmail.com>
2023-10-03 18:04:12 +0900

    fix: 식재료 포함 레시피 목록 반환 로직 변경 (string -> JPA Query)
    Former-commit-id: 596e9fa061f66310040fce9ab670bfcb8b8000e8 [formerly faa5f836ea0241dac697bdac55119fdf53516852]
    Former-commit-id: 26342e70afd0945d92a8e1e85f8a5a398ff04d18

--- BE/src/main/java/com/tls/recipe/repository/RecipeIngredientRepository.java
+++ BE/src/main/java/com/tls/recipe/repository/RecipeIngredientRepository.java
@@ -4,7 +4,10 @@ import com.tls.recipe.entity.composite.RecipeIngr;
 import com.tls.recipe.entity.single.Recipe;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngr, Long> {
     List<RecipeIngr> findByRecipeId(Recipe recipeId);
+    @Query("SELECT DISTINCT r FROM RecipeIngr ri JOIN ri.recipeId r WHERE ri.RecipeIngrName LIKE %?1%")
+    List<Recipe> findDistinctRecipesByIngredientNameContains(String ingredientName);
 }
