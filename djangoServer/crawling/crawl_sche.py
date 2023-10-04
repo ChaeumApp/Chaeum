a8cd9803b32c3656272ccbaa62fe174edc574614
박건후 <gunhoo2016@gmail.com>
2023-10-05 00:18:01 +0900

    fix: 크롤링 예외처리 추가
    Former-commit-id: 17eda488250b0e9355ea761bdaf5a38dc44874d3 [formerly 8bdfeddcfe4164762fe8dd3dbdae10e40be949f8]
    Former-commit-id: 79f6dc7116b09db952f80c44fb685bd8eab9b5b5

--- djangoServer/crawling/crawl_sche.py
+++ djangoServer/crawling/crawl_sche.py
@@ -14,7 +14,7 @@ def start():
 
     scheduler.add_job(
         main,
-        trigger=CronTrigger(hour="00", minute="01"),
+        trigger=CronTrigger(hour="00", minute="20"),
         id = "main",
         max_instances=1,
         replace_existing=True
